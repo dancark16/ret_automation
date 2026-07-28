f83ca9c844f7bec4add9111a2d1d56a23293a79e
dancark16 <danial.cv.24@gmail.com>
2026-07-27 19:06:04 -0500

    Sacar aparte el frontend

--- frontend-retencion/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://tu-app.railway.app',
-    wsUrl: 'wss://tu-app.railway.app/ws/progress'
-};
--- frontend-retencion/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8000',
-    wsUrl: 'ws://localhost:8000/ws/progress'
-};
